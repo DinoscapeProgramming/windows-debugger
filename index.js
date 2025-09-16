dd448308f31c47120566af4758157e33990cfc6c
DinoscapeProgramming <emailfromdinoscape@gmail.com>
2025-09-16 17:02:48 +0200

    feat: use uuid as repl password for security

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const net = require("net");
 const util = require("util");
 const repl = require("repl");
+const crypto = require("crypto");
 const domain = require("domain");
 const childProcess = require("child_process");
 
 const replDomain = domain.create();
 
-module.exports = ({ title = "Windows Debugger", default: defaultValue, eval: evalFunction = eval }) => replDomain.run(() => {
+module.exports = ({ title = "Windows Debugger", default: defaultValue, eval: evalFunction = eval, password = crypto.randomUUID() } = {}) => replDomain.run(() => {
   console.assert(process.platform === "win32", "This module only works on Windows.");
 
   const server = net.createServer((socket) => {
-    repl.start({
-      input: socket,
-      output: socket,
-      terminal: false,
-      prompt: "> ",
-      eval: (command, _, __, callback) => {
-        try {
-          callback(null, (command.trim()) ? evalFunction(command) : defaultValue);
-        } catch (error) {
-          callback(error);
-        };
-      },
-      writer: (output) => util.inspect(output, { colors: true, depth: null })
+    socket.once("data", (data) => {
+      if (data.toString().trim() !== password) return socket.destroy();
+
+      repl.start({
+        input: socket,
+        output: socket,
+        terminal: false,
+        prompt: "> ",
+        eval: (command, _, __, callback) => {
+          try {
+            callback(null, (command.trim()) ? evalFunction(command) : defaultValue);
+          } catch (error) {
+            callback(error);
+          };
+        },
+        writer: (output) => util.inspect(output, { colors: true, depth: null })
+      });
     });
   });
 
@@ -33,6 +38,7 @@ module.exports = ({ title = "Windows Debugger", default: defaultValue, eval: eva
       `$host.UI.RawUI.WindowTitle='${title.replace(/'/g, "''")}'; node -e '${`
         const net = require('net');
         const socket = net.connect(${server.address().port.toString()}, 'localhost');
+        socket.write(\`\${process.env.REPL_PASSWORD}\\n\`);
         socket.pipe(process.stdout);
         process.stdin.pipe(socket);
       `.replace(/\n/g, " ").replace(/'/g, "''")}'; exit`
@@ -40,7 +46,13 @@ module.exports = ({ title = "Windows Debugger", default: defaultValue, eval: eva
       cwd: process.cwd(),
       shell: true,
       detached: true,
-      stdio: "ignore"
+      stdio: "ignore",
+      env: {
+        ...process.env,
+        ...{
+          REPL_PASSWORD: password
+        }
+      }
     });
   });
 });
